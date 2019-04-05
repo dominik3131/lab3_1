168ede8f8703909627eb1c64cb9449003781b78f
Dominik <rakdominikrak@gmail.com>
2019-04-05 10:55:13 +0200

    shouldUseAddMethodOnce()

--- src/test/java/lab3_1/AddProductCommandHandlerTests.java
+++ src/test/java/lab3_1/AddProductCommandHandlerTests.java
@@ -1,11 +1,16 @@
 package lab3_1;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Date;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.internal.util.reflection.Whitebox;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
@@ -28,7 +33,7 @@ public class AddProductCommandHandlerTests {
     private ReservationRepository reservationRepository;
     private ProductRepository productRepository;
     private Reservation reservation;
-    private Reservation spy;
+    private Reservation reservationSpy;
     private Product product;
     private AddProductCommandHandler handler;
     private AddProductCommand productCommand;
@@ -41,7 +46,7 @@ public class AddProductCommandHandlerTests {
     @Before
     public void setUp() {
         handler = new AddProductCommandHandler();
-        productCommand = new AddProductCommand(new Id("1"), new Id("2"), 3);
+        productCommand = new AddProductCommand(new Id("1"), new Id("2"), 5);
         reservationRepository = mock(ReservationRepository.class);
         productRepository = mock(ProductRepository.class);
         clientRepository = mock(ClientRepository.class);
@@ -50,7 +55,7 @@ public class AddProductCommandHandlerTests {
                 new Date());
         product = spy(new Product(Id.generate(), new Money(10), "Product", ProductType.STANDARD));
         equivalent = new Product(Id.generate(), new Money(20), "Equivalent", ProductType.STANDARD);
-        spy = spy(reservation);
+        reservationSpy = spy(reservation);
         systemContext = new SystemContext();
         Whitebox.setInternalState(handler, "reservationRepository", reservationRepository);
         Whitebox.setInternalState(handler, "productRepository", productRepository);
@@ -58,5 +63,21 @@ public class AddProductCommandHandlerTests {
         Whitebox.setInternalState(handler, "clientRepository", clientRepository);
         Whitebox.setInternalState(handler, "systemContext", systemContext);
         client = new Client();
+    }
+
+    @Test
+    public void shouldLoadReservationOnce() {
+        when(reservationRepository.load(any(Id.class))).thenReturn(reservationSpy);
+        when(productRepository.load(any(Id.class))).thenReturn(product);
+        handler.handle(productCommand);
+        verify(reservationRepository, times(1)).save(any(Reservation.class));
+    }
+
+    @Test
+    public void shouldUseAddMethodOnce() {
+        when(reservationRepository.load(any(Id.class))).thenReturn(reservationSpy);
+        when(productRepository.load(any(Id.class))).thenReturn(product);
+        handler.handle(productCommand);
+        verify(reservationSpy, times(1)).add(product, 5);
     }
 }
